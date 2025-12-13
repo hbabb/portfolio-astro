003b6f7b60ff3680bd509851312eaaceedfcae8e
Heath Babb <hbabb@heath-babb.dev>
2025-12-13 13:21:59 -0500

    chore: commit after format and lint

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,24 @@
-import markdoc from "@astrojs/markdoc";
-import mdx from "@astrojs/mdx";
-import netlify from "@astrojs/netlify";
-import react from "@astrojs/react";
-import sitemap from "@astrojs/sitemap";
-import icon from "astro-icon";
+import markdoc from '@astrojs/markdoc';
+import mdx from '@astrojs/mdx';
+import netlify from '@astrojs/netlify';
+import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
+import icon from 'astro-icon';
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		react(),
-		markdoc(),
-		mdx(),
-		sitemap(),
-		icon({
-			include: {
-				"fa7-brands": ["*"],
-			},
-		}),
-	],
-	adapter: netlify(),
+  integrations: [
+    react(),
+    markdoc(),
+    mdx(),
+    sitemap(),
+    icon({
+      include: {
+        'fa7-brands': ['*'],
+      },
+    }),
+  ],
+  adapter: netlify(),
 });
